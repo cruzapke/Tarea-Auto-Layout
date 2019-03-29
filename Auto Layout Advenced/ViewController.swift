fa9c8513e42a99363bac3b07fbeecc89352f327d
KELLY CRUZ APARICIO D3 <kelly_diplomado@macbook-23.local>
2019-03-29 17:03:55 -0600

    Initial Commit

---
+++ Auto Layout Advenced/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Advenced
+//
+//  Created by KELLY CRUZ APARICIO D3 on 3/29/19.
+//  Copyright © 2019 ioslab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
